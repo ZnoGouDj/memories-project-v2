de01fc3be036749458a237406e4d5293b92df79f
ZnoGouDj <znogoud3@gmail.com>
2022-04-24 13:20:01 +0200

    Search & Pagination finish

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -9,6 +9,8 @@ const Posts = ({ setCurrentId }) => {
   const { posts, isLoading } = useSelector(state => state.posts);
   const classes = useStyles();
 
+  if (!posts.length && !isLoading) return 'No posts';
+
   return isLoading ? (
     <CircularProgress />
   ) : (
